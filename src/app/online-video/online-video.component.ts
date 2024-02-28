6471491d1103c190747e8dc0116d0411c90e3f02
realzeepro <realzeepro@gmail.com>
2024-02-28 14:21:18 +0500

    Added button to capture thubnail.

--- src/app/online-video/online-video.component.ts
+++ src/app/online-video/online-video.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-online-video',
-  standalone: true,
-  imports: [],
   templateUrl: './online-video.component.html',
-  styleUrl: './online-video.component.css'
+  styleUrls: ['./online-video.component.css']
 })
 export class OnlineVideoComponent {
+  @ViewChild('video') videoElement!: ElementRef;
+  @ViewChild('canvas') canvas!: ElementRef;
 
+  constructor() { }
+
+  capture() {
+    const canvas = this.canvas.nativeElement;
+    const video1 = this.videoElement.nativeElement;
+        
+    // Pause the video
+    if (!video1.paused) {
+      video1.pause();
+    }
+    
+    // Draw the paused frame on canvas
+    canvas.getContext('2d').drawImage(video1, 0, 0, video1.videoWidth, video1.videoHeight);
+
+  }
+
+  clearCanvas() {
+    const canvas = this.canvas.nativeElement;
+    const ctx = canvas.getContext('2d');
+
+    // Clear the entire canvas
+    ctx.clearRect(2, 2, canvas.width, canvas.height);
+  }
 }
